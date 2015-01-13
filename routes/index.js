f097765ccbca76adf0aeed930c2503f79951e73f
Michael Lannin <mlannin23@gmail.com>
2015-01-12 21:45:38 -0800

    Set up two routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Shatter Magazine' });
+});
+
+/* GET article page */
+router.get('/article', function(req, res) {
+  res.render('article', { title: 'Article Title' });
 });
 
 module.exports = router;
